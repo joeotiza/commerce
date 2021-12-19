f4748bb9c82d516f9ddd804d99803fea4f689e7c
joeotiza <73652815+joeotiza@users.noreply.github.com>
2021-12-19 07:05:19 +0300

    Add files via upload

---
+++ function/addproduct.php
@@ -0,0 +1,12 @@
+<?php
+    $q1 = $conn->query("INSERT INTO product (name, price, image, brand, category)
+    VALUES ('$name','$price','$img', '$brand', '$category')");
+
+    $q0 = $conn->query("SELECT * FROM `product` WHERE `name`='$name' AND `price`='$price'");
+    $rq0=$q0->fetch_array();
+    $productid = $rq0['productid'];
+
+    $q2 = $conn->query("INSERT INTO stock (productid, quantity) VALUES ('$productid','$quantity')");
+
+    $q3 = $conn->query("ALTER TABLE `transactionitems` ADD `$productid` TINYINT(1) NOT NULL DEFAULT '0'");
+?>
